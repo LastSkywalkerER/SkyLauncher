652fa2fb138dd06c340fe4daecc888ff8ca0dd8d
LastSkywalker <maxdr1998@gmail.com>
2025-02-05 17:52:16 +0300

    fix config format

--- src/main/modules/config/configuration.ts
+++ src/main/modules/config/configuration.ts
@@ -1,8 +1,4 @@
-import { config } from 'dotenv'
-
 import { arch, platform } from '../../../shared/constants'
-
-config()
 
 const defaultConfig = {
   hardware: {
@@ -10,16 +6,14 @@ const defaultConfig = {
     architecture: arch
   },
   s3client: {
-    endPoint: process.env.MAIN_VITE_MINIO_ENDPOINT || import.meta.env.MAIN_VITE_MINIO_ENDPOINT,
-    port: Number(process.env.MAIN_VITE_MINIO_PORT || import.meta.env.MAIN_VITE_MINIO_PORT),
-    useSSL:
-      (process.env.MAIN_VITE_MINIO_USESSL || import.meta.env.MAIN_VITE_MINIO_USESSL) === 'true',
-    accessKey: process.env.MAIN_VITE_MINIO_ACCESSKEY || import.meta.env.MAIN_VITE_MINIO_ACCESSKEY,
-    secretKey: process.env.MAIN_VITE_MINIO_SECRETKEY || import.meta.env.MAIN_VITE_MINIO_SECRETKEY
+    endPoint: import.meta.env.MAIN_VITE_MINIO_ENDPOINT,
+    port: Number(import.meta.env.MAIN_VITE_MINIO_PORT),
+    useSSL: import.meta.env.MAIN_VITE_MINIO_USESSL === 'true',
+    accessKey: import.meta.env.MAIN_VITE_MINIO_ACCESSKEY,
+    secretKey: import.meta.env.MAIN_VITE_MINIO_SECRETKEY
   },
-  curseForgeApiKey:
-    process.env.MAIN_VITE_CURSEFORGE_APIKEY || import.meta.env.MAIN_VITE_CURSEFORGE_APIKEY,
-  javaBaseUrl: process.env.MAIN_VITE_JAVA_BASE_URL || import.meta.env.MAIN_VITE_JAVA_BASE_URL
+  curseForgeApiKey: import.meta.env.MAIN_VITE_CURSEFORGE_APIKEY,
+  javaBaseUrl: import.meta.env.MAIN_VITE_JAVA_BASE_URL
 }
 
 export default (): typeof defaultConfig => defaultConfig
